033249673457a0d3422edbcd0ee6e0ecee141a00
gomezarnol555-lgtm <gomezarnol555@gmail.com>
2026-09-23 11:24:30 -0600

    Add files via upload

---
+++ app_calidad.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from configuracion import APP_NAME,FORCE_RESET_ADMIN,ADMIN_PASS
+from base_datos import init_db
+from seguridad import reset_admin,_instalar_filtro_entradas_streamlit
+from interfaz import *
+st.set_page_config(page_title=APP_NAME,page_icon="✅",layout="wide",initial_sidebar_state="collapsed")
+_instalar_filtro_entradas_streamlit()
+def main():
+    init_state();init_db()
+    if FORCE_RESET_ADMIN and ADMIN_PASS: reset_admin()
+    user=login();cambiar_password_obligatoria();styles(False)
+    a,b=st.columns([.19,.81],gap='large')
+    with a:left_menu()
+    with b:
+        topbar(user);p=st.session_state.page
+        if p=='Inicio':page_inicio()
+        elif p=='Nuevo registro':page_registro()
+        elif p=='Consulta y descarga':page_consulta()
+        elif p=='Muestras de retención':page_muestras_retencion()
+        elif p=='Entrega de turno':page_entrega_turno()
+        elif p=='Catálogos':page_catalogos()
+        elif p=='Usuarios':page_usuarios()
+        elif p=='Auditoría':page_auditoria()
+if __name__=='__main__':main()
